e3e92ab69d6747bbc61dff1f428f5dd2e0ecbe38
Xtao_dada <xtao@xtaolink.cn>
2020-05-16 22:15:02 +0800

    🚩 优化翻译

--- pagermaid/modules/prune.py
+++ pagermaid/modules/prune.py
@@ -7,7 +7,7 @@ from pagermaid.listener import listener
 
 
 @listener(outgoing=True, command="prune",
-          description="以此命令回复某条消息，将删除最新一条消息至该条消息之间的所有消息。限制：基于消息 ID 的 1000 条消息，大于 1000 条将不作出应答。（非群组管理员只删除自己的消息）")
+          description="以此命令回复某条消息，将删除最新一条消息至该条消息之间的所有消息。限制：基于消息 ID 的 1000 条消息，大于 1000 条可能会触发删除消息过快限制。（非群组管理员只删除自己的消息）")
 async def prune(context):
     """ Purge every single message after the message you replied to. """
     if not context.reply_to_msg_id:
@@ -33,7 +33,7 @@ async def prune(context):
 
 
 @listener(outgoing=True, command="selfprune",
-          description="删除当前对话您发送的特定数量的消息。（倒序）",
+          description="删除当前对话您发送的特定数量的消息。限制：基于消息 ID 的 1000 条消息，大于 1000 条可能会触发删除消息过快限制。入群消息非管理员无法删除。（倒序）当数字足够大时即可实现删除所有消息。",
           parameters="<数量>")
 async def selfprune(context):
     """ Deletes specific amount of messages you sent. """
@@ -58,7 +58,7 @@ async def selfprune(context):
 
 
 @listener(outgoing=True, command="yourprune",
-          description="删除当前对话您回复用户所发送的特定数量的消息。（倒序、需要删除消息权限）",
+          description="删除当前对话您回复用户所发送的特定数量的消息。限制：基于消息 ID 的 1000 条消息，大于 1000 条可能会触发删除消息过快限制。（倒序、需要删除消息权限）当数字足够大时即可实现删除所有消息。",
           parameters="<数量>")
 async def yourprune(context):
     """ Deletes specific amount of messages someone sent. """
