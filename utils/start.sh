4847dea439d74d8995727cfd6c4b79779f04c9fb
Kevin Sanchez <33132228+KevinZonda@users.noreply.github.com>
2020-08-11 10:51:16 +0800

    ✏️ Format code (#25)

--- utils/start.sh
+++ utils/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-while true; do
-    clear;
-    python3 -m pagermaid;
-    echo '正在重新启动...';
-    sleep 1;
+while :
+do
+  clear
+  python3 -m pagermaid
+  echo '正在重新启动...'
+  sleep 1
 done
