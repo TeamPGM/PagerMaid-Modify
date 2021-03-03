bbc1dca904406d03f548b25009d381dce142f14e
Pentacene <48641773+Pentacene@users.noreply.github.com>
2021-03-04 00:51:22 +0800

    修复多重log问题

--- pagermaid/__init__.py
+++ pagermaid/__init__.py
@@ -20,9 +20,10 @@ logs = getLogger(__name__)
 logging_format = "%(levelname)s [%(asctime)s] [%(name)s] %(message)s"
 logging_handler = StreamHandler()
 logging_handler.setFormatter(ColoredFormatter(logging_format))
-logs.addHandler(logging_handler)
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.ERROR)
+root_logger.addHandler(logging_handler)
 basicConfig(level=INFO)
-getLogger('telethon').setLevel(INFO)
 logs.setLevel(INFO)
 
 try:
