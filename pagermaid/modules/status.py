3a01234b2e48e45112906040396a609ace2ca299
Xtao_dada <xtao@xtaolink.cn>
2021-03-28 10:02:13 +0800

    ✨ 增加对话数统计 (#53)
    * ✨ 增加对话数统计
    
    * ♻️ 将对话总数延后显示
    
    * 🐛 修复文本格式错误

--- pagermaid/modules/status.py
+++ pagermaid/modules/status.py
@@ -69,16 +69,20 @@ async def tty(context):
 @listener(is_plugin=False, outgoing=True, command="status",
           description="输出 PagerMaid-Modify 的运行状态。")
 async def status(context):
-    database = "Connected" if redis_status() else "Disconnected"
-    await context.edit(
-        f"**PagerMaid-Modify 运行状态** \n"
+    database = "在线" if redis_status() else "离线"
+    text = (f"**PagerMaid-Modify 运行状态** \n"
         f"主机名: `{uname().node}` \n"
         f"主机平台: `{platform}` \n"
         f"Kernel 版本: `{uname().release}` \n"
         f"Python 版本: `{python_version()}` \n"
-        f"Library 版本: `{telethon_version.__version__}` \n"
-        f"数据库状态: `{'Connected' if redis_status() else 'Disconnected'}`"
+        f"Telethon 版本: `{telethon_version.__version__}` \n"
+        f"数据库状态: `{database}`"
     )
+    await context.edit(text)
+    dialogs = await context.client.get_dialogs()
+    dialogs = len(dialogs)
+    text += f"\n对话总数: `{dialogs}`"
+    await context.edit(text)
 
 
 @listener(is_plugin=False, outgoing=True, command="speedtest",
