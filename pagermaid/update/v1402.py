8b7fc317a671ae813e109b0feac785ee6c6da76f
xtaodada <xtao@xtao.de>
2025-07-29 18:34:09 +0800

    :ambulance: Fix docker make data folder

---
+++ pagermaid/update/v1402.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+data = Path("data")
+data.mkdir(exist_ok=True)
+
+
+def rename(old: Path, need_backup: bool = True):
+    if old.exists():
+        old_file_name = old.name
+        new = data / old_file_name
+        if need_backup and new.exists():
+            datetime_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            backup = data / f"{new.stem}.{datetime_str}{new.suffix}"
+            shutil.move(new, backup)
+        shutil.move(old, new)
+
+
+# move file
+# session
+session = Path("pagermaid.session")
+rename(session)
+# config
+config = Path("config.yml")
+rename(config)
+# docker
+docker = Path("install.lock")
+rename(docker, need_backup=False)
+# delete file
+# log
+log = Path("pagermaid.log.txt")
+if log.exists():
+    log.unlink()
